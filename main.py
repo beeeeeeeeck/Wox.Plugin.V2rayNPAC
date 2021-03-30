4c8e3ffac707196cfab01387c67f37e61e08ea5e
Beck Lin <runbeck@163.com>
2021-03-30 23:34:38 +0800

    Adding github sync up

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import os
 import json
 import re
 import subprocess
+from github import Github
+from datetime import datetime
+from win10toast import ToastNotifier
 
 with open(os.path.abspath("config.json"), encoding = "utf-8") as f:
     data = json.load(f)
     v2rayNDirPath = data["v2rayN-Path"]
+    GithubAccessToken = data["Github-Token"]
+    GithubRepoName = data["Github-Repo-Name"]
+    GithubRepoBranchName = data["Github-Repo-Branch-Name"]
+    GithubRepoFileName = data["Github-Repo-File-Name"]
 
 pluginHomeDirPath = os.path.dirname(os.path.realpath(__file__))
 configFilePath = os.path.join(v2rayNDirPath, 'guiNConfig.json') if len(v2rayNDirPath) > 0 else ''
@@ -22,7 +29,7 @@ class V2rayNPACEditor(Wox):
             results.append({
                 "Title": "Configuration Error",
                 "SubTitle": "Add v2rayN home dir to config.json in {}".format(pluginHomeDirPath),
-                "IcoPath":"Images/app.png",
+                "IcoPath": "Images/app.png",
                 "ContextData": "ctxData",
                 "JsonRPCAction": {
                     'method': 'open_plugin_directory',
@@ -35,10 +42,23 @@ class V2rayNPACEditor(Wox):
             results.append({
                 "Title": "Configuration Error",
                 "SubTitle": "v2rayN config file NOT FOUND - {}".format(configFilePath),
-                "IcoPath":"Images/app.png",
+                "IcoPath": "Images/app.png",
                 "ContextData": "ctxData",
                 "JsonRPCAction": {
                     'method': 'open_v2rayN_dir_directory',
+                    'dontHideAfterAction': False
+                }
+            })
+            return results
+
+        if len(query) > 0 and query == "sync" and len(GithubAccessToken) > 0:
+            results.append({
+                "Title": "Sync up user rules",
+                "SubTitle": "Sync up with github storage",
+                "IcoPath": "Images/github.png",
+                "ContextData": "ctxData",
+                "JsonRPCAction": {
+                    'method': 'sync_up_user_rules',
                     'dontHideAfterAction': False
                 }
             })
@@ -58,7 +78,7 @@ class V2rayNPACEditor(Wox):
                 results.append({
                     "Title": "Add new rule",
                     "SubTitle": "Add this rule - {}".format(query),
-                    "IcoPath":"Images/app.png",
+                    "IcoPath": "Images/app.png",
                     "ContextData": "ctxData",
                     "JsonRPCAction": {
                         'method': 'take_action_4_new_rule',
@@ -71,7 +91,7 @@ class V2rayNPACEditor(Wox):
             results.append({
                 "Title": existingRule,
                 "SubTitle": "Remove this rule",
-                "IcoPath":"Images/app.png",
+                "IcoPath": "Images/app.png",
                 "ContextData": "ctxData",
                 "JsonRPCAction": {
                     'method': 'take_action_4_existing_rule',
@@ -88,7 +108,7 @@ class V2rayNPACEditor(Wox):
         results.append({
             "Title": "Context menu entry",
             "SubTitle": "Data: {}".format(data),
-            "IcoPath":"Images/app.png"
+            "IcoPath": "Images/app.png"
         })
         return results
 
@@ -116,6 +136,8 @@ class V2rayNPACEditor(Wox):
             # print(f"killed v2rayN")
             subprocess.Popen([os.path.join(v2rayNDirPath, "v2rayN.exe")])
 
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "New rule has been added and v2rayN is just restarted !", icon_path = "Images/success_notification.ico", duration = 10, threaded = True)
+
         return None
 
     def take_action_4_existing_rule(self, existingRule):
@@ -141,6 +163,8 @@ class V2rayNPACEditor(Wox):
             # print(f"killed v2rayN")
             subprocess.Popen([os.path.join(v2rayNDirPath, "v2rayN.exe")])
 
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Existing rule has been removed and v2rayN is just restarted !", icon_path = "Images/success_notification.ico", duration = 10, threaded = True)
+
         return None
 
     def open_plugin_directory(self):
@@ -148,8 +172,71 @@ class V2rayNPACEditor(Wox):
 
     def open_v2rayN_dir_directory(self):
         if not os.path.isdir(v2rayNDirPath):
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Configuration Error for v2rayN directory path !", icon_path = "Images/fail_notification.ico", duration = 10, threaded = True)
             return
         subprocess.Popen('explorer "{}"'.format(v2rayNDirPath))
+
+    def sync_up_user_rules(self):
+        toaster = ToastNotifier()
+        if not GithubAccessToken or len(GithubAccessToken) == 0:
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Configuration Error for Github Access Token !", icon_path = "Images/fail_notification.ico", duration = 10, threaded = True)
+            return
+        if not GithubRepoName or len(GithubRepoName) == 0:
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Configuration Error for Github Repository Name, it should be full name started with the account name !", icon_path = "Images/fail_notification.ico", duration = 10, threaded = True)
+            return
+        if not GithubRepoBranchName or len(GithubRepoBranchName) == 0:
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Configuration Error for Github Repository Branch Name, it should be existing !", icon_path = "Images/fail_notification.ico", duration = 10, threaded = True)
+            return
+        if not GithubRepoFileName or len(GithubRepoFileName) == 0:
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Configuration Error for Github Repository File Name, it should be in the root directory !", icon_path = "Images/fail_notification.ico", duration = 10, threaded = True)
+            return
+
+        # using an access token
+        g = Github(GithubAccessToken)
+        repo = g.get_repo(GithubRepoName)
+        repoFiles = repo.get_contents("", ref = GithubRepoBranchName)
+        existingRuleFile = None
+        while repoFiles:
+            repoFile = repoFiles.pop(0)
+            if len(repoFile.name) > 0 and repoFile.name.endswith(GithubRepoFileName):
+                existingRuleFile = repoFile
+        if not existingRuleFile:
+            emptyRules = {}
+            emptyRules['rules'] = list()
+            repo.create_file(GithubRepoFileName, "Created default rules.json file", json.dumps(emptyRules, indent = 4), branch = GithubRepoBranchName)
+
+        ruleFile = repo.get_contents(GithubRepoFileName, ref = GithubRepoBranchName)
+        ruleFileContent = ruleFile.decoded_content.decode('utf8').replace("'", '"')
+        ruleFileData = json.loads(ruleFileContent)
+
+        # Opening v2rayN config JSON file
+        f = open(configFilePath,)
+        # returns JSON object as a dictionary
+        configFileData = json.load(f)
+        # Closing file
+        f.close()
+
+        ruleSet = set()
+        ruleSet.update(ruleFileData['rules'])
+        ruleSet.update(configFileData["userPacRule"])
+        ruleList = list(ruleSet)
+        ruleList.sort()
+        ruleFileData['rules'] = ruleList
+        configFileData["userPacRule"] = ruleList
+
+        now = datetime.now() # current date and time
+        currentDateTime = now.strftime("%m/%d/%Y %H:%M:%S")
+        repo.update_file(ruleFile.path, "Rules file synced at {}".format(currentDateTime), json.dumps(ruleFileData, indent = 4), ruleFile.sha, branch = GithubRepoBranchName)
+
+        with open(configFilePath, 'w+', encoding = 'utf-8') as f:
+            json.dump(configFileData, f, ensure_ascii = False, indent = 4)
+
+            os.system('taskkill /IM "v2rayN.exe" /F')
+            # print(f"killed v2rayN")
+            subprocess.Popen([os.path.join(v2rayNDirPath, "v2rayN.exe")])
+
+            toaster.show_toast("Wox.Plugin.v2rayNPAC", "Rules has been synced up with github storage and v2rayN is just restarted !", icon_path = "Images/success_notification.ico", duration = 10, threaded = True)
+
 
 if __name__ == "__main__":
     V2rayNPACEditor()
--- readme.md
+++ readme.md
@@ -1,44 +1,41 @@
 # Wox - Plugin for v2rayN PAC editing and syncing
 
-### Flow for plugin
- - Define `plugin.json`
- - Make `main.py`
+### Quick start
+Pull down this repository at your Wox plugin directory which is usually in **[USER_DIR\AppData\Local\Wox\app-x.x.xxx\Plugins]()**.
+ - `pac xxxx.com` to add new user rule
+ - `pac xxx` and then select existing rule to remove it
+ - `pac sync` to sync up with github storage (Note that the [github access configuration](#config) should be set before this)
 
 ---
-### Define `plugin.json`
- 
+
+### <a name="config"></a>Configuration - define `config.json` in the plugin directory
+
  ```
- {
-    "ID":"4f134ec8-7d55-11e9-bc5b-2a86e4085a59",
-    "ActionKeyword":"h",
-    "Name":"Hello World Python",
-    "Description":"Hello World",
-    "Author":"Wox",
-    "Version":"1.0",
-    "Language":"python",
-    "Website":"https://github.com/Wox-launche/Wox",
-    "IcoPath":"Images\\app.png",
-    "ExecuteFileName":"main.py"
+{
+    "v2rayN-Path":"D:/v2ray/v2rayN",
+    "Github-Token":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "Github-Repo-Name":"xxxxxx/pac-user-rules",
+    "Github-Repo-Branch-Name":"main",
+    "Github-Repo-File-Name":"rules.json"
 }
  ```
- 
- > `ID`: defines uniqueness of plugin and can be generated from `https://www.uuidgenerator.net/`
- >
- > `ActionKeyword`: to enable the plugin in launcher
- > 
- > `Name`: Name of Plugin
- > 
- > `Description`: Short Description of the plugin
- >
- > `Author`: Name of creator of plugin
- >
- > `Version`: Version of Plugin
- >
- > `Language`: "python"
- >
- > `Website`: Website link to plugin
- >
- > `IcoPath`: Path to icon
- >
- > `ExecuteFileName`: The main file from where the execution to plugin can start
 
+
+### Prerequisites
+
+ > `Wox`: Install Wox from `https://github.com/Wox-launcher/Wox/releases`
+ >
+ > `Python`: this is required by Wox - `https://www.python.org/downloads/windows/`
+ >
+ > `Python libs`: since this plugin is implemented by Python, we need the following python libs installed
+ > - `pip install PyGithub`
+ > - `pip install win10toast`
+ >
+ > `Github Repository`:
+ > 1. Create a new private repository with the default **README.md** file
+ > 2. Create github access token for this plugin
+ > 3. Update plugin [configuration](#config) accordingly
+
+---
+
+Start to play the plugin and enjoy the convenient !
